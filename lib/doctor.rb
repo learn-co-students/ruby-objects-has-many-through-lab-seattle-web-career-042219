c36fd89db4cc6043185f10aba09cfb277ea637a3
github username <github email address>
2019-04-26 12:42:37 -0700

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+
+  def patients
+    self.appointments.map { |appointment|
+      # binding.pry
+      appointment.patient
+      # binding.pry
+    }
+  end
+
+  def self.all
+    @@all
+  end
+end
